5dd51f2ca7d0617c6d5d91e8f5010ecd9cfa1187
mutouzha22-spec <mutouzha22@gmail.com>
2026-06-09 08:56:24 +0800

    v9

---
+++ sw.js
@@ -0,0 +1,28 @@
+'use strict';
+const CACHE = 'reading-app-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.open(CACHE).then(cache =>
+      cache.match(e.request).then(cached => {
+        if (cached) return cached;
+        return fetch(e.request).then(res => {
+          cache.put(e.request, res.clone());
+          return res;
+        });
+      })
+    )
+  );
+});
